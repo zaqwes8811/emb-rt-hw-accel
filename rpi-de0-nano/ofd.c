5ae10d78d79acff22d9ba08880d762d653b292f3
zaqwes8811 <igor.a.lugansky@gmail.com>
2015-10-25 13:32:09 +0300

    dvrs:     somehow compile in eclipse. looks terrible

--- rpi-de0-nano/ofd.c
+++ rpi-de0-nano/ofd.c
@@ -3,39 +3,37 @@
 #include <linux/kernel.h>
 #include <linux/cdev.h>
 #include <linux/gpio.h>
-
-// some code from
-/*
-* raspi_gpio_driver.c - GPIO Linux device driver for Raspberry Pi B
-* rev 2.0 platform
-* Author: Vu Nguyen <quangngmetro@gmail.com>
-* Version: 0.2
-* License: GPL
-*/
-
-struct raspi_gpio_dev {
-	struct cdev cdev;
-	struct gpio pin;
-	//enum state state;
-	//enum direction dir;
-	bool irq_perm;
-	unsigned long irq_flag;
-	unsigned int irq_counter;
-	//spinlock_t lock;
-};
+#include <linux/fs.h>
 
 // fixme: make as in video but without direct calls HW
 // fixme: how use IRQ
 //
 // fixme: try with poll() interface. only one can open
 
-static int __init ofd_init(void) /* Constructor */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Anil Kumar Pugalia <email_at_sarika-pugs_dot_com>");
+MODULE_DESCRIPTION("Our First Driver");
+
+static int majorNumber;
+static char message[256];
+static short size_of_message;
+static int numberOpens = 0;
+static struct class* ebbcharClass = NULL;
+
+static int dev_open(struct inode*, struct file *);
+
+static struct file_operations fops =
+{
+		.open = dev_open
+};
+
+static int __init ofd_init(void)
 {
     printk(KERN_INFO "Namaskar: ofd registered");
     return 0;
 }
 
-static void __exit ofd_exit(void) /* Destructor */
+static void __exit ofd_exit(void)
 {
     printk(KERN_INFO "Alvida: ofd unregistered");
 } 
@@ -43,6 +41,3 @@ static void __exit ofd_exit(void) /* Destructor */
 module_init(ofd_init);
 module_exit(ofd_exit);
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Anil Kumar Pugalia <email_at_sarika-pugs_dot_com>");
-MODULE_DESCRIPTION("Our First Driver");

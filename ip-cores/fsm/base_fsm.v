5edfacf2c8c138ad61d689fdbfa197377249a772
zaqwes8811 <igor.a.lugansky@gmail.com>
2016-02-14 16:04:11 +0300

    verilog: some steps fort

--- ip-cores/fsm/base_fsm.v
+++ ip-cores/fsm/base_fsm.v
@@ -1,58 +1,38 @@
-// читаем тактовой частотой, а потом выдает сигналы реже
-//   тогда такт на spi будет прямой
+// FIXME: очень плохое APi
 
-// нужно поймать данные? а если они нулевые?
-// это слейву нужно посстанавливать клок
-
-//localparam 
-
-// Нужен инверсный сигнал такта, но буфферизовывать 
-//   основной такт не стоит. Сделаю регист на выходе.
-
-// сигнал от микросхемы будет асинхронный, так что нужнен 
-//   синхронизатор для предоптвр. метастабильности
-
-// Art of Hardware....
-
-// Не нужно изгяляться над клоком
-// выход счетчика и регистр на выходе, чтобы не дребезжал
-// нужно защелкнуть выход
-
-// Multiply always blocks
-//   http://electronics.stackexchange.com/questions/29601/why-cant-regs-be-assigned-to-multiple-always-blocks-in-synthesizable-verilog
-//   http://electronics.stackexchange.com/questions/29553/how-are-verilog-always-statements-implemented-in-hardware
-
-//"One important restriction that pops up is that every reg variable 
-//can only be assigned to in at most one always 
-//statement. In other words, regs have affinity to always blocks."
-//
-// Т.е. писать можно только в одном, но читать в любом?
+`include "const.v"
 
 module splitter( 
-	clk, rst_a, ena, 
+	clk, rst, div, 
 	sclk_n, cs_n,
 
-	from_device
+	from_device,
+	just_test,
+	just_test_bus
 );
 
-input clk, ena, rst_a;  // это не простые сигналы, нужно очень хорошо
+input clk, rst;  // это не простые сигналы, нужно очень хорошо
 	// понимать как с ними иметь дело
+
+input [`W-1:0] div;
+output [7:0] just_test_bus;
 output reg sclk_n;
 output reg cs_n;
 input from_device;  // нужно защелкнуть
+output just_test;
 
-reg clk_n_work;
-reg cs_n_work;
+reg clk_n_w;
+reg cs_n_w;
 reg clk_tmp;
 wire clk_mask;
+reg internal_enable;
+reg [`W-1:0] div_cntr;
+reg [`W-1:0] div_cntr_nxt;
 
-
-// state
-reg [4:0] cntr_curr;
-reg [4:0] cntr_next;
+reg [7:0] cntr_curr;
+reg [7:0] cntr_next;
 reg [3:0] state_curr;
 reg [3:0] state_next;
-// state
 
 localparam IDLE = 2'b00, 
 	CS_N_WAIT = 2'b01,
@@ -60,51 +40,98 @@ localparam IDLE = 2'b00,
 	S3 = 2'b11;
 
 always @(*) begin
+	// can declare 'reg' here
 	state_next = state_curr;
 	cntr_next = cntr_curr;
-	cs_n_work = 1;
+	cs_n_w = 1;
 	case( state_curr )
 		IDLE: begin
-			cs_n_work = 1;
+			cs_n_w = 1;
 			cntr_next = 0;
 			state_next = CS_N_WAIT;
 		end
 		CS_N_WAIT: begin
-			if( cntr_curr < 16 ) begin
-				cs_n_work = 0;				
-				cntr_next = cntr_next + 1'b1;
-			end
-			else begin
-				state_next = IDLE;
+			if( internal_enable ) begin
+				if( cntr_curr < `PKG_SIZE * 2 ) begin
+					cs_n_w = 0;				
+					cntr_next = cntr_next + 1'b1;
+				end
+				else begin
+					state_next = IDLE;
+				end
 			end
 		end
 	endcase
 end
 
-// если отдельные блоки, то могут быть гонки
-always @(posedge clk or posedge rst_a) begin
-	if (rst_a) begin
-		state_curr <= IDLE;	
-		clk_n_work <= 1;
-	end 
-	else begin
-		state_curr <= state_next;
+// детектировать сигнал нужно основным клоком, т.к. мы 
+//   тактируем им напрямую, ширина стробирующий импльсов в один
+//   такт основого клока
+//
+// Обойтись бы без детектирования 
 
-		cntr_curr <= cntr_next;
-		// кажется так нельзя
-		clk_n_work <= clk_n_work + 1'b1;  // ??
+// если отдельные блоки, то могут быть гонки
+// fixme: async rst is bad looks lime in fpga
+always @( posedge clk ) begin
+	// fIXME: если синхр. сброс, то ресет станет
+	//   триггером, т.е. входом триггера
+	// http://www.sunburst-design.com/papers/CummingsSNUG2003Boston_Resets.pdf
+	if( rst ) begin
+		state_curr <= IDLE;	
+	end 
+	else if( internal_enable ) begin
+		state_curr <= state_next;
 	end
 end
 
-always @( posedge clk or posedge rst_a ) begin
-	if( rst_a ) begin
+always @( posedge clk ) begin
+	if ( rst ) begin
+		cntr_curr <= 0;
+		clk_n_w <= 1;
+	end
+	else if( internal_enable ) begin
+		cntr_curr <= cntr_next;
+		clk_n_w <= clk_n_w + 1'b1;	
+	end
+end
+
+// output
+always @( posedge clk ) begin
+	if( rst ) begin
 		sclk_n <= 1;
 		cs_n <= 1;
 	end
-	else begin
-		sclk_n <= ~(clk_n_work & ~(cs_n & cs_n_work));
-		cs_n <= cs_n_work;
+	else if( internal_enable ) begin
+		sclk_n <= ~(clk_n_w & ~(cs_n & cs_n_w));
+		cs_n <= cs_n_w;
 	end
 end
+
+// divider
+// fixme: Это clk-gating и правильно ли это вообще?
+always @( * ) begin
+	internal_enable = 0;
+	div_cntr_nxt = div_cntr;
+	if( div_cntr == 3'b11-1 ) begin  // счет с нуля!
+		internal_enable = 1;
+		div_cntr_nxt = 0;
+	end
+	else begin
+		div_cntr_nxt = div_cntr + 1'b1;
+	end
+end
+
+// нужно в два раза быстрее
+always @( posedge clk ) begin
+	if( rst )begin
+		div_cntr <= 0;		
+	end
+	else begin	
+		div_cntr <= div_cntr_nxt;
+	end
+end
+
+assign just_test = internal_enable;
+assign just_test_bus = div_cntr;
 
 endmodule
--- ip-cores/fsm/testbench_base_fsm.v
+++ ip-cores/fsm/testbench_base_fsm.v
@@ -1,24 +1,39 @@
 //`timescale 1 ns / 100 ps
 `timescale 10ns / 1ns
 
-`define WIDTH 8
+`include "const.v"
 
 module sum_tb;
 
 wire sclk_n;
 wire cs_n;
-reg rst_a;
+reg rst;
 reg from_device;
+wire just_test;
+wire [7:0] just_test_bus;
+
+wire [`W-1:0] div;
 
 // stimuls
 reg clk;
 
+// fixme: пока просто по модулю 8 
+assign div = 6;//`DIV_MAX - 1;
+
 initial begin
 	clk = 0;
-	rst_a = 0;
+	// rst = 0;
 	from_device = 0;
-	#10 rst_a = 1;
-	#10 rst_a = 0;
+	// #10 rst = 1;
+	// #10 rst = 0;
+
+// fixme: понять и возможно переделать
+// http://www.sunburst-design.com/papers/CummingsSNUG2003Boston_Resets.pdf
+// !!!
+	rst <= 1; // time 0 nonblocking assignment
+ 	@(posedge clk); // Wait to get past time 0
+ 	@(negedge clk) rst = 0; // rst_n low for one clock cycle
+
 end
 
 always
@@ -29,10 +44,12 @@ always @( posedge clk ) begin
 	from_device <= $random;  // можно просто генерить биты
 	// а там как попадет
 end
-	
+
+
 splitter s0( 
-	.clk( clk ), .rst_a( rst_a ), .ena( 1'b1 ), 
+	.clk( clk ), .rst( rst ), .div( div ), 
 	.sclk_n(sclk_n), .cs_n( cs_n ), 
-	.from_device(from_device) );
+	.from_device(from_device), .just_test( just_test ),
+	.just_test_bus( just_test_bus ) );
 
 endmodule

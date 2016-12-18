ba45fc55b3445fe27d51e5a185e59b58ef16d86e
zaqwes8811 <igor.a.lugansky@gmail.com>
2016-12-18 21:10:46 +0300

    save

--- homography_warp_tb.v
+++ homography_warp_tb.v
@@ -4,6 +4,9 @@
 // fixme: memory access unregular - сделать пока просто, с поворотом
 //  потом думать об оптимизации - пока сложно представить
 
+// rect:
+// https://www.researchgate.net/profile/Ratiba_Fellag/publication/273348053_Survey_on_Image_Rectification_on_FPGA/links/54ff043f0cf2672e2240f3a5.pdf
+
 module homography_warp_tb;
 
 endmodule

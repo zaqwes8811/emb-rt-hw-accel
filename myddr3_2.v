415edceba4fb37b9a4aa47e91672aaf2351b5fa3
zaqwes8811 <igor.a.lugansky@gmail.com>
2017-04-02 10:25:53 +0300

    add some about ddrX

---
+++ myddr3_2.v
@@ -0,0 +1,13 @@
+ 
+
+// похоже нужен контроллер и сама модель памяти 
+// https://alteraforum.com/forum/showthread.php?t=50393
+
+// https://www.altera.com/ja_JP/pdfs/literature/hb/external-memory/emi_ddr3up_ug.pdf
+
+// pure sim:
+// https://www.altera.com.cn/zh_CN/pdfs/literature/hb/external-memory/emi_debug_verify.pdf
+
+
+// QSys
+// http://www.alterawiki.com/wiki/Reference_Design_-_Cyclone_V_Hard_Memory_Controller_with_Avalon_MM_data_width_expanded_for_User_ECC
